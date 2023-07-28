857f4c4493b2521c49c6828757ac686cba4106ed
Javid <cavidgulmaliyev491995@gmail.com>
2023-07-29 03:37:20 +0400

    add media section and index page

--- js/script.js
+++ js/script.js
@@ -911,7 +911,197 @@ upButton?.addEventListener("click", () => {
   });
 });
 
+;// CONCATENATED MODULE: ./src/js/modules/same.js
+class Same {
+  /** @type {NodeListOf<HTMLDivElement>} */
+  #elements;
+  #breakpoint;
+  #breakpointType;
+  #cssProperties = {
+    height: "--same-height",
+    width: "--same-width"
+  };
+  #matchMedia;
+  /** @type {ResizeObserver} */
+  #observer;
+  #same;
+
+  /** @param {SameOptions} options */
+  constructor(options) {
+    this.#breakpoint = options.breakpoint;
+    this.#elements = document.querySelectorAll(options.selector);
+    this.#same = options.same;
+
+    if (this.#breakpoint) {
+      this.#breakpointType = options.breakpointType ?? "min";
+      this.#matchMedia = matchMedia(`(${this.#breakpointType}-width: ${this.#breakpoint}px)`);
+    }
+
+    if (this.#elements.length && this.#same) {
+      this.#init();
+    }
+  }
+
+  #init() {
+    if (this.#breakpoint) {
+      if (this.#matchMedia.matches) this.#resizeObserver();
+
+      this.#matchMedia.addEventListener("change", event => {
+        const { matches } = event;
+
+        if (matches) {
+          this.#resizeObserver();
+        } else {
+          this.#observer?.disconnect();
+
+          if (this.#same === "height") {
+            this.#removeHeightProperty();
+          } else if (this.#same === "width") {
+            this.#removeWidthProperty();
+          } else {
+            this.#removeBothProperties();
+          }
+        }
+      });
+    } else {
+      this.#resizeObserver();
+    }
+  }
+
+  #resizeObserver() {
+    this.#observer = new ResizeObserver(entries => {
+      entries.forEach(entry => {
+        if (this.#same === "height") {
+          this.#removeHeightProperty();
+
+          const maxHeight = Math.max(...[...this.#elements].map($element => {
+            return parseFloat(getComputedStyle($element).height);
+          }));
+
+          this.#elements.forEach($element => {
+            $element.style.setProperty(this.#cssProperties.height, `${maxHeight || 0}px`);
+          });
+        } else if (this.#same === "width") {
+          this.#removeWidthProperty();
+
+          const maxWidth = Math.max(...[...this.#elements].map($element => {
+            return parseFloat(getComputedStyle($element).width);
+          }));
+
+          this.#elements.forEach($element => {
+            $element.style.setProperty(this.#cssProperties.width, `${maxWidth || 0}px`);
+          });
+        } else {
+          this.#removeBothProperties();
+
+          const maxHeight = Math.max(...[...this.#elements].map($element => {
+            return parseFloat(getComputedStyle($element).height);
+          }));
+
+          const maxWidth = Math.max(...[...this.#elements].map($element => {
+            return parseFloat(getComputedStyle($element).width);
+          }));
+
+          this.#elements.forEach($element => {
+            $element.style.setProperty(this.#cssProperties.height, `${maxHeight || 0}px`);
+            $element.style.setProperty(this.#cssProperties.width, `${maxWidth || 0}px`);
+          });
+        }
+      });
+    });
+
+    this.#elements.forEach($element => {
+      this.#observer.observe($element);
+    });
+  }
+
+  #removeHeightProperty() {
+    this.#elements.forEach($element => {
+      $element.style.removeProperty(this.#cssProperties.height);
+    });
+  }
+
+  #removeWidthProperty() {
+    this.#elements.forEach($element => {
+      $element.style.removeProperty(this.#cssProperties.width);
+    });
+  }
+
+  #removeBothProperties() {
+    this.#elements.forEach($element => {
+      $element.style.removeProperty(this.#cssProperties.height);
+      $element.style.removeProperty(this.#cssProperties.width);
+    });
+  }
+}
+
+
+
+;// CONCATENATED MODULE: ./src/js/scripts/scripts/main-media.js
+
+
+const sameMediaItems = new Same({
+  selector: ".main-media-photos__item, .main-media-videos__item",
+  same: "height",
+  breakpoint: 1441,
+  breakpointType: "min",
+});
+
+;// CONCATENATED MODULE: ./src/js/scripts/scripts/youtube.js
+const youtubeBlocks = document.querySelectorAll(".youtube");
+
+if (youtubeBlocks.length) {
+  const script = document.createElement('script');
+  const firstScriptTag = document.getElementsByTagName('script')[0];
+
+  script.src = "https://www.youtube.com/iframe_api";
+  firstScriptTag.parentNode.insertBefore(script, firstScriptTag);
+
+  youtubeBlocks?.forEach(youtube => {
+    const { video } = youtube.dataset;
+    const button = youtube.querySelector(".youtube__play");
+
+    if (video && button) {
+      const block = document.createElement("div");
+      const img = document.createElement("img");
+
+      img.src = `https://i.ytimg.com/vi/${video}/sddefault.jpg`;
+      youtube.insertAdjacentElement("afterbegin", img);
+      youtube.insertAdjacentElement("afterbegin", block);
+
+      button.addEventListener("click", () => {
+        img.remove();
+        button.remove();
+        youtube.classList.add("youtube--played");
+        onYouTubeIframeAPIReady();
+
+        function onYouTubeIframeAPIReady() {
+          new YT.Player(block, {
+            videoId: video,
+            playerVars: {
+              "autoplay": 1,
+              "rel": 0,
+              "playsinline": 1,
+            },
+            events: {
+              'onReady': onPlayerReady,
+            },
+          });
+        }
+
+        function onPlayerReady(event) {
+          if (matchMedia("(hover: none)").matches) event.target.mute();
+
+          event.target.playVideo();
+        }
+      });
+    }
+  });
+}
+
 ;// CONCATENATED MODULE: ./src/js/scripts/scripts.js
+
+
 
 
 
